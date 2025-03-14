c156dde78ff7f8f249b977b2f5b9b9a7b5098e4c
SuperAdmin <SuperAdmin@E5470-7VX7QC2>
2025-03-14 22:22:39 +0500

    Add project files.

---
+++ MUHID KHAN LAB 5/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUHID_KHAN_LAB_5
+{
+    public class ShoppingCart
+    {
+        public int cartID;
+        public string customername;
+        public double totalprice;
+        public Product[] products;
+        public int count;
+        public ShoppingCart()
+        {
+            cartID = 1;
+            customername = " ";
+            totalprice = 0;
+            products = new Product[20];
+            count = 0;
+        }
+        
+        public void addproduct(Product product)
+        {
+            products[count] = product;
+            count++;
+            totalprice += product.price;
+        }
+        
+        public void displayProducts()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (products[i] != null)
+                {
+
+
+                    Console.WriteLine("the product ID is {0}", products[i].productid);
+                    Console.WriteLine("the product name is {0}", products[i].name);
+                    Console.WriteLine("the product's price is {0}", products[i].price);
+                    Console.WriteLine("-------------------------");
+                }
+                else {
+                    continue;    }
+
+            }
+            Console.WriteLine("the total Bill is : {0}",totalprice);
+
+        }
+        public void removeProduct(int key) 
+        {
+            for (int i = 0; i < count; i++) 
+            {
+                if (products[i].productid == key)
+                {
+                    totalprice -= products[i].price;
+                    products[i]= null;
+                    
+                }
+            }
+
+        }
+    }
+        public class Product
+        {
+            public int productid;
+            public string name;
+            public double price;
+            public Product(int productid, string name, double price)
+            {
+                this.productid = productid;
+                this.name = name;
+                this.price = price;
+            }
+        public void display()
+        {
+            Console.WriteLine("the product ID is {0}", productid);
+            Console.WriteLine("the product name is {0}", name);
+            Console.WriteLine("the product's price is {0}", price);
+        }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            Product product1 = new Product(1, "Laptop", 90000);
+            Product product2 = new Product(2, "Gamepad", 4500);
+            Product product3 = new Product(3, "Phone", 45000);
+            Product product4 = new Product(4, "Ipad", 50000);
+            ShoppingCart cart = new ShoppingCart();
+            
+            Console.WriteLine("enter your name: ");
+            cart.customername= Console.ReadLine();
+           
+            int choice = 0;
+         
+            while (choice!=-1)
+            {
+                Console.WriteLine("-------------------");
+                Console.WriteLine("|Choose a product: |");
+                Console.WriteLine("-------------------");
+                product1.display();
+                Console.WriteLine("-------------------------");
+                product2.display();
+                Console.WriteLine("-------------------------");
+                product3.display();
+                Console.WriteLine("-------------------------");
+                product4.display();
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("enter the ID of the desired product(-1 to exit): ");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+
+                {
+                    case 1:
+                        cart.addproduct(product1);
+                        break;
+                    case 2:
+                        cart.addproduct(product2);
+                        break;
+                    case 3:
+                        cart.addproduct(product3);
+                        break;
+                    case 4:
+                        cart.addproduct(product4);
+                        break;
+                    default:
+                        break;
+
+                }
+                Console.Clear();
+            }
+            Console.Clear();
+            Console.WriteLine("now your cart have following products");
+            cart.displayProducts();
+            Console.WriteLine("do you want to remove a products");
+            string choice_rem= Console.ReadLine();
+            Console.Clear();
+            if (choice_rem == "yes")
+            {
+                Console.WriteLine("enter the ID of the product you want to remove: ");
+                int key= int.Parse(Console.ReadLine());
+                cart.removeProduct(key);
+            }
+            Console.WriteLine("now your cart have following products");
+            cart.displayProducts();
+
+
+
+
+        }
+        }
+    }
+
---
+++ Q2/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    public class Company
+    {
+        public string Name;
+        public Employee[] employees;
+        public int count;
+        public Company()
+        {
+            count = 0;
+            Name = "MUHID KHAN\'s Company";
+            employees = new Employee[10];
+        }
+        public void addemployee(Employee employee)
+        {
+            employees[count] = employee;
+            count++;
+        }
+        public void removeEmployee(int key)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (employees[i].EmployeeID == key)
+                {
+
+                    employees[i] = null;
+
+                }
+            }
+        }
+        
+        public void remove(int id)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (employees[i].EmployeeID == id)
+                {
+                    employees[i]= null;
+                }
+            }
+        }
+        public void display()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (employees[i] != null) 
+                {
+                    Console.WriteLine("the employee ID is : {0}", employees[i].EmployeeID);
+                    Console.WriteLine("the employee' First name  is : {0}", employees[i].FirstName);
+                    Console.WriteLine("the employee's Last name  is : {0}", employees[i].LastName);
+                    Console.WriteLine("the employee's Department is : {0}", employees[i].Department);
+                    Console.WriteLine("the employee's Salary is : {0}", employees[i].Salary);
+                    Console.WriteLine("------------------------------------------------");
+                }
+                
+                else 
+                {
+                    continue;
+                }
+            }
+        }
+    }
+
+    public class Employee
+    {
+        public int EmployeeID;
+        public string FirstName;
+        public string LastName;
+        public double Salary;
+        public string Department;
+        public Employee(int employeeID, string firstName, string lastName, double salary, string department)
+        {
+            EmployeeID = employeeID;
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+            Department = department;
+        }
+            public void display()
+            {
+                Console.WriteLine("the employee ID is : {0}", EmployeeID);
+                Console.WriteLine("the employee' First name  is : {0}",FirstName);
+                Console.WriteLine("the employee's Last name  is : {0}",LastName);
+                Console.WriteLine("the employee's Department is : {0}",Department);
+                Console.WriteLine("the employee's Salary is : {0}",Salary);
+            }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(1, "John", "Doe", 50000, "IT");
+            Employee employee2 = new Employee(2, "Jane", "Smith", 55000, "HR");
+            Employee employee3 = new Employee(3, "Mike", "Brown", 60000, "Finance");
+            Employee employee4 = new Employee(4, "Emily", "Davis", 52000, "Marketing");
+            Employee employee5 = new Employee(5, "David", "Wilson", 58000, "Operations");
+            Company company = new Company();
+            company.addemployee(employee1);
+            company.addemployee(employee2);
+            company.addemployee(employee3);
+            company.addemployee(employee4);
+            company.addemployee(employee5);
+            Console.WriteLine("here is the list of employees: ");
+            Console.WriteLine("------------------------------------");
+            company.display();
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("do you want to remove an employee??");
+            string decision= Console.ReadLine();
+            Console.Clear();
+            if (decision == "yes")
+            {
+                Console.WriteLine("Enter the ID of the employee you wish to remove");
+                int key = int.Parse (Console.ReadLine());
+                company.remove(key);
+            }
+            Console.WriteLine("here is the list of employees after removing the employee: ");
+            Console.WriteLine("------------------------------------");
+            company.display();
+            Console.WriteLine("------------------------------------");
+
+
+
+        }
+    }
+}
